144d5b10d26db97de913d2a2abd0302a40504770
y-hamada <y-hamada-tg@nec.com>
2021-03-29 12:07:58 +0900

    Fix razzle config to supress warings.

--- examples/react-app/razzle.config.js
+++ examples/react-app/razzle.config.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs')
+const resolveRequest = require('razzle-dev-utils/resolveRequest');
+
 module.exports = {
   plugins: [{
     name: 'typescript',
@@ -10,11 +13,35 @@ module.exports = {
     }
   }],
 
-  modifyWebpackConfig: ({ webpackConfig }) => {
+  modifyWebpackOptions: ({ options: { webpackOptions } }) => {
     // This is required to import cross-kube as ES module
     // https://github.com/jaredpalmer/razzle/issues/842
-    delete webpackConfig.externals;
+    webpackOptions.notNodeExternalResMatch = (request, context) => {
+      const result = request.match(/cross-kube[/\\]lib[/\\]/)
+      return result != null
+    }
+    return webpackOptions
+  },
 
+  modifyWebpackConfig: ({ webpackConfig, options: { webpackOptions } }) => {
+    // This is required to import cross-kube as ES module
+    // https://github.com/jaredpalmer/razzle/issues/842
+    const allowedPackages = ['cross-kube'];
+    const allowedPackagePaths = allowedPackages.map(packageName =>
+      fs.realpathSync('./node_modules/' + packageName)
+    );
+    const tsRuleIndex = webpackConfig.module.rules.findIndex(
+      rule => rule.use && rule.use[0].loader && rule.use[0].loader.includes('ts-loader')
+    );
+    if (tsRuleIndex === -1) {
+      throw Error(
+        'This component assumes that you are using ts-loader. If you are not using it, then you might need to check and test code for how would it work with other loaders'
+      );
+    }
+    webpackConfig.module.rules[tsRuleIndex] = {
+      ...webpackConfig.module.rules[tsRuleIndex],
+      include: [...webpackConfig.module.rules[tsRuleIndex].include, ...allowedPackagePaths]
+    };
 
     // For debugging
     // config.mode = 'development'
